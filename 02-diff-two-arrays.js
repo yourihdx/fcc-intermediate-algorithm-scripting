2a19daa683f382cdb4fb85ea09809e12c2fadbcc
Youri Hdx <yourihdx@gmail.com>
2018-02-14 23:28:54 +0800

    just added a backbone and a task as a comment

---
+++ 02-diff-two-arrays.js
@@ -0,0 +1,12 @@
+// diff two arrays
+// Compare two arrays and return a new array with any items only found in one of the two given arrays, but not both. In other words, return the symmetric difference of the two arrays.
+
+function diffArray(arr1, arr2) {
+
+    var newArr = [];
+    // Same, same; but different.
+    return newArr;
+    
+  }
+  
+  diffArray([1, 2, 3, 5], [1, 2, 3, 4, 5]);
