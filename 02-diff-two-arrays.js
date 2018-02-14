26a8bd2afa4429725f7d7a184fe63ec586277a79
Youri Hdx <yourihdx@gmail.com>
2018-02-15 00:57:06 +0800

    Just to sync, but it's not working

--- 02-diff-two-arrays.js
+++ 02-diff-two-arrays.js
@@ -3,10 +3,18 @@
 
 function diffArray(arr1, arr2) {
 
-    var newArr = [];
-    // Same, same; but different.
-    return newArr;
+  var concat_arr = arr1.concat(arr2);
+  // Same, same; but different.
+  for (var index = 0; index < arr2.length; index++) {
+    for (var jndex = 0; jndex < concat_arr.length; jndex++){
+      // console.log(arr1[index], ' == ', concat_arr[jndex], ' is ', arr1[index] == concat_arr[jndex]);
+      var newArr = arr2.filter(num => arr2[index] != concat_arr[jndex]);
+    }
+    
+  }
+  console.log(newArr);
+    // return newArr;
     
   }
   
-  diffArray([1, 2, 3, 5], [1, 2, 3, 4, 5]);
+diffArray([1, 2, 3, 5], [1, 2, 3, 4, 5]);
