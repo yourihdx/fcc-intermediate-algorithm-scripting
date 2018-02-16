a90e4cc5c492f4a7e9a744d125e0c72442366d85
Youri Hdx <yourihdx@gmail.com>
2018-02-16 17:02:36 +0800

    left previous (3rd) for later refactoring, and started 4th

---
+++ 03-1-roman-numeral-converter.js
@@ -0,0 +1,13 @@
+// Convert the given number into a roman numeral.
+// using   Array.prototype.splice()
+//         Array.prototype.indexOf()
+//         Array.prototype.join()
+
+// for numbers from 1 to 3999 
+function convertToRoman(num) {
+    return num;
+   }
+   
+   convertToRoman(36);
+
+convertToRoman(24);
