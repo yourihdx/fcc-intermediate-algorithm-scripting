1d1f4cc71e126d561c962a35d15efc159dd1b573
Youri Hdx <yourihdx@gmail.com>
2018-08-27 21:51:44 +0800

    ex 011 - using spread operator to evaluate arrays in-place

---
+++ ES6/011-ES6-Use-the-Spread-Operator-to-Evaluate-Arrays-In-Place.js
@@ -0,0 +1,9 @@
+// Copy all contents of arr1 into another array arr2 using the spread operator.
+
+const arr1 = ['JAN', 'FEB', 'MAR', 'APR', 'MAY'];
+let arr2;
+(function() {
+  "use strict";
+  arr2 = [...arr1]; // change this line
+})();
+console.log(...arr2);
