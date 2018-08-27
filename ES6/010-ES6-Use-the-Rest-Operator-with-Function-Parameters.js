89dee01f650f3cc9ee62708f115d02ec0fa2d451
Youri Hdx <yourihdx@gmail.com>
2018-08-27 21:01:58 +0800

    ex 010: using the rest operator with function parameters

---
+++ ES6/010-ES6-Use-the-Rest-Operator-with-Function-Parameters.js
@@ -0,0 +1,9 @@
+// Modify the function sum so that it uses the rest operator and it works in the same way with any number of parameters.
+
+const sum = (function() {
+  "use strict";
+  return function sum(...args) {
+    return args.reduce((a, b) => a + b, 0);
+  };
+})();
+console.log(sum(1, 2, 3)); // 6
