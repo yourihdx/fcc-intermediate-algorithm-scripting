7a8f3460584bf816bb8efbf9650f31841c029d75
Youri Hdx <yourihdx@gmail.com>
2018-08-30 21:08:18 +0800

    ex 017: creating strings using template literals

---
+++ ES6/017-ES6-Create-Strings-using-Template-Literals.js
@@ -0,0 +1,37 @@
+// Use template literal syntax with backticks to display each entry of the result object's failure array. Each entry should be wrapped inside an li element with the class attribute text-warning, and listed within the resultDisplayArray.
+
+
+const result = {
+  success: ["max-length", "no-amd", "prefer-arrow-functions"],
+  failure: ["no-var", "var-on-top", "linebreak"],
+  skipped: ["id-blacklist", "no-dup-keys"]
+};
+function makeList(arr) {
+  "use strict";
+
+  // change code below this line
+  const resultDisplayArray = [`<li class="text-warning">${arr[0]}</li>`,
+    `<li class="text-warning">${arr[1]}</li>`,
+    `<li class="text-warning">${arr[2]}</li>`];
+  // change code above this line
+
+  return resultDisplayArray;
+}
+/**
+ * makeList(result.failure) should return:
+ * [ `<li class="text-warning">no-var</li>`,
+ *   `<li class="text-warning">var-on-top</li>`, 
+ *   `<li class="text-warning">linebreak</li>` ]
+ **/
+const resultDisplayArray = makeList(result.failure);
+console.log(resultDisplayArray);
+
+// A lot of things happened there.
+
+// Firstly, the example uses backticks (`), not quotes (' or "), to wrap the string.
+
+// Secondly, notice that the string is multi-line, both in the code and the output. This saves inserting within strings.
+
+// The ${variable} syntax used above is a placeholder. Basically, you won't have to use concatenation with the + operator anymore. To add variables to strings, you just drop the variable in a template string and wrap it with ${ and }. Similarly, you can include other expressions in your string literal, for example ${a + b}.
+
+// This new way of creating strings gives you more flexibility to create robust strings.
