cecf532fec165c4582e74d29b51282402cb4a126
Youri Hdx <yourihdx@gmail.com>
2018-09-02 11:29:50 +0800

    ex 024: using asterics to import everything from a file

---
+++ ES6/024-ES6-Use-asterics-to-Import-Everything-from-a-File.js
@@ -0,0 +1,9 @@
+// The code below requires the contents of a file, "capitalize_strings", found in the same directory as it, imported. Add the appropriate import * statement to the top of the file, using the object provided.
+
+"use strict";
+
+import * as imported from "capitalize_strings";
+
+
+// import * as object_with_name_of_your_choice from "file_path_goes_here"
+// object_with_name_of_your_choice.imported_function
