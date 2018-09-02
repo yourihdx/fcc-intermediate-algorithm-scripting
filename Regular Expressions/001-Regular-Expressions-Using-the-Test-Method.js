9bdbcaa7e9e6841bdb0756a94b3d03411529906a
Youri Hdx <yourihdx@gmail.com>
2018-09-02 14:14:26 +0800

    ex 001: using the test method for regular expressions

---
+++ Regular Expressions/001-Regular-Expressions-Using-the-Test-Method.js
@@ -0,0 +1,7 @@
+// Apply the regex myRegex on the string myString using the .test() method.
+
+let myString = "Hello, World!";
+let myRegex = /Hello/;
+let result = myRegex.test(myString); // Change this line
+
+// JavaScript has multiple ways to use regexes. One way to test a regex is using the .test() method. The .test() method takes the regex, applies it to a string (which is placed inside the parentheses), and returns true or false if your pattern finds something or not.
