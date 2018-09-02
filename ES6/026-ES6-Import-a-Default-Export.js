1c0499cc2ccbd22f9023371c9f932e77bd300c92
Youri Hdx <yourihdx@gmail.com>
2018-09-02 14:04:33 +0800

    ex 026: importing a default export

---
+++ ES6/026-ES6-Import-a-Default-Export.js
@@ -0,0 +1,8 @@
+// In the following code, please import the default export, subtract, from the file "math_functions", found in the same directory as this file.
+
+
+"use strict";
+import subtract from "math_functions";
+subtract(7,4);
+
+// The syntax differs in one key place - the imported value, add, is not surrounded by curly braces, {}. Unlike exported values, the primary method of importing a default export is to simply write the value's name after import.
