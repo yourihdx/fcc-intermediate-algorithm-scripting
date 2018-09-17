2a930714df30a030d909d2907648bfd514ed0dea
yourihdx <yourihdx@gmail.com>
2018-09-17 22:16:15 +0800

    ex 014: finding characters with lazy matching

--- Regular Expressions/009-Regular-Expressions-Match-Letters-of-the-Alphabet.js
+++ Regular Expressions/009-Regular-Expressions-Match-Letters-of-the-Alphabet.js
@@ -1,11 +1,15 @@
-// Match all the letters in the string quoteSample.
+// Fix the regex /<.*>/ to return the HTML tag <h1> and not the text "<h1>Winter is coming</h1>". Remember the wildcard . in a regular expression matches any character.
 
-// Note
-// Be sure to match both upper - and lowercase letters.
-let quoteSample = "The quick brown fox jumps over the lazy dog.";
-let alphabetRegex = /[a-z]/gi; // Change this line
-let result = quoteSample.match(alphabetRegex); // Change this line
+let text = "<h1>Winter is coming</h1>";
+let myRegex = /<.*?1>/; // Change this line
+let result = text.match(myRegex);
 
-// Inside a character set, you can define a range of characters to match using a hyphen character: -.
 
-// For example, to match lowercase letters a through e you would use[a - e].
+
+// In regular expressions, a greedy match finds the longest possible part of a string that fits the regex pattern and returns it as a match. The alternative is called a lazy match, which finds the smallest possible part of the string that satisfies the regex pattern.
+
+// You can apply the regex /t[a-z]*i/ to the string "titanic". This regex is basically a pattern that starts with t, ends with i, and has some letters in between.
+
+// Regular expressions are by default greedy, so the match would return ["titani"]. It finds the largest sub-string possible to fit the pattern.
+
+// However, you can use the ? character to change it to lazy matching. "titanic" matched against the adjusted regex of /t[a-z]*?i/ returns ["ti"].
