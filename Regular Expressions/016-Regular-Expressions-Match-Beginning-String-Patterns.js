0257b7442919239eca6deb99bf901c2faace4460
yourihdx <yourihdx@gmail.com>
2018-09-18 22:12:23 +0800

    ex 016: matching beginning string patterns

---
+++ Regular Expressions/016-Regular-Expressions-Match-Beginning-String-Patterns.js
@@ -0,0 +1,10 @@
+// Use the caret character in a regex to find "Cal" only in the beginning of the string rickyAndCal.
+
+let rickyAndCal = "Cal and Ricky both like racing.";
+let calRegex = /^Cal/; // Change this line
+let result = calRegex.test(rickyAndCal);
+
+
+// Regular expressions are also used to search for patterns in specific positions in strings.
+
+// In an earlier challenge, you used the caret character (^) inside a character set to create a negated character set in the form [^thingsThatWillNotBeMatched]. Outside of a character set, the caret is used to search for patterns at the beginning of strings.
