4e6cf4fe833dbbe91358643b830aa28a55a7dd75
Youri Hdx <yourihdx@gmail.com>
2018-11-15 21:44:09 +0800

    ex 024: searching for non-whitespace chars with '\S'

---
+++ Regular Expressions/024-Regular-Expressions-Match-Non-Whitespace-Characters.js
@@ -0,0 +1,8 @@
+// Change the regex countNonWhiteSpace to look
+// for multiple non - whitespace characters in a string.
+
+let sample = "Whitespace is important in separating words";
+let countNonWhiteSpace = /\S/g; // Change this line
+let result = sample.match(countNonWhiteSpace);
+
+// Search for non - whitespace using\ S, which is an uppercase s.This pattern will not match whitespace, carriage return, tab, form feed, and new line characters.You can think of it being similar to the character class[ ^ \r\ t\ f\ n\ v].
