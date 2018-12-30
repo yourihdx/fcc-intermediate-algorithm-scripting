4c5951fe5112783277732f1d4a260ae69ffb0039
yourihdx <yourihdx@gmail.com>
2018-12-31 00:39:02 +0800

    ex 010: modifying an object nested within an object

---
+++ Basic Data Structures/010-Modify-an-Object-Nested-Within-an-Object.js
@@ -0,0 +1,18 @@
+// Here we've defined an object, userActivity, which includes another object nested within it. You can modify properties on this nested object in the same way you modified properties in the last challenge. Set the value of the online key to 45.
+
+let userActivity = {
+    id: 23894201352,
+    date: 'January 1, 2017',
+    data: {
+      totalUsers: 51,
+      online: 42
+    }
+  };
+  
+  // change code below this line
+  userActivity.data.online = 45;
+  // change code above this line
+  
+  console.log(userActivity);
+
+  // (c) use the same notation to access any depth level
