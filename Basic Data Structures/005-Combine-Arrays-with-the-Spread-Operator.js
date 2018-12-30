ede8e4350ed54357762c0ef239babc2f7c2fbe08
yourihdx <yourihdx@gmail.com>
2018-12-30 15:00:50 +0800

    ex 006: checking for the presence of an element with indexOf

--- Basic Data Structures/005-Combine-Arrays-with-the-Spread-Operator.js
+++ Basic Data Structures/005-Combine-Arrays-with-the-Spread-Operator.js
@@ -10,7 +10,7 @@ function spreadOut() {
   console.log(spreadOut());
 
 
-  /*
+/*
 
 
   Another huge advantage of the spread operator, is the ability to combine arrays, or to insert all the elements of one array into another, at any index. With more traditional syntaxes, we can concatenate arrays, but this only allows us to combine arrays at the end of one, and at the start of another. Spread syntax makes the following operation extremely simple:
