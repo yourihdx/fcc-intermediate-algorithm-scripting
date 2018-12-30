4a3dbc6fa50693a8c895f09cda93f1fe9cc1e228
yourihdx <yourihdx@gmail.com>
2018-12-31 00:49:48 +0800

    ex 012: using the delete keyword to remove object properties

---
+++ Basic Data Structures/012-Use-the-delete-Keyword-to-Remove-Object-Properties.js
@@ -0,0 +1,29 @@
+// Use the delete keyword to remove the oranges, plums, and strawberries keys from the foods object.
+
+let foods = {
+    apples: 25,
+    oranges: 32,
+    plums: 28,
+    bananas: 13,
+    grapes: 35,
+    strawberries: 27
+  };
+  
+  // change code below this line
+  delete foods.oranges; 
+  delete foods.plums; 
+  delete foods.strawberries;
+  // change code above this line
+  
+  console.log(foods);
+
+
+
+
+/*
+
+Let's revisit our foods object example one last time. If we wanted to remove the apples key, we can remove it by using the delete keyword like this:
+
+delete foods.apples;
+
+*/
