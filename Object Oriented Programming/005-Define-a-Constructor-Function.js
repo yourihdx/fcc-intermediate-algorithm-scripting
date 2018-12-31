73f253916b1201278bc7eb2f864c32498c50dcd0
yourihdx <yourihdx@gmail.com>
2018-12-31 17:14:09 +0800

    e005: defining a constructor function

---
+++ Object Oriented Programming/005-Define-a-Constructor-Function.js
@@ -0,0 +1,7 @@
+// Create a constructor, Dog, with properties name, color, and numLegs that are set to a string, a string, and a number, respectively.
+
+function Dog(){
+    this.name = "Klyopa";
+    this.color = "black";
+    this.numLegs = 4;
+  }
