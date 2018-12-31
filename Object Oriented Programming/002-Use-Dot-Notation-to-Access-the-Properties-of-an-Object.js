411b461437936d6bc75ee3418024ca529fba9541
yourihdx <yourihdx@gmail.com>
2018-12-31 16:58:10 +0800

    e002: using dot notation to access the properties of an object

---
+++ Object Oriented Programming/002-Use-Dot-Notation-to-Access-the-Properties-of-an-Object.js
@@ -0,0 +1,8 @@
+// Print both properties of the dog object below to your console.
+
+let dog = {
+    name: "Spot",
+    numLegs: 4
+  };
+  // Add your code below this line
+  console.log(dog.name, dog.numLegs);
