e01ef17d1acce05235f55898f4cd031199e725b8
yourihdx <yourihdx@gmail.com>
2019-02-28 08:16:10 +0800

    adding #014 exercise which was missing

--- Intermediate Algorithm Scripting/05 Spinal Tap Case.js
+++ Intermediate Algorithm Scripting/05 Spinal Tap Case.js
@@ -1,7 +1,7 @@
 function spinalCase(str) {
     // "It's such a fine line between stupid, and clever."
     // --David St. Hubbins
-    var strArr = str.split(/[A-Z][a-z]*/);
+    var strArr = str.split(/[A-Z][a-z]*|\W+/);
 
     strArr = strArr.map(function (element) {
         return element.toLowerCase();
@@ -16,3 +16,7 @@ console.log(spinalCase('This Is Spinal Tap'));
 console.log(spinalCase("thisIsSpinal<wbr>Tap"));
 console.log(spinalCase("The_Andy_<wbr>Griffith_Show"));
 console.log(spinalCase("AllThe-small Things"));
+
+/* Итак, можем ли мы иметь моноширинный
+	шрифт, для того,чтобы сравнивать it with thet
+*/
