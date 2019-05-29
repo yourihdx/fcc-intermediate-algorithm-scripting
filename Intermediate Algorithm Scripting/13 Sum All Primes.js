3ed4a53c9967f958e81cff8a868df3fa36c4f717
yourihdx <yourihdx@gmail.com>
2019-05-30 07:10:04 +0800

    syncing last file's barebone

---
+++ Intermediate Algorithm Scripting/13 Sum All Primes.js
@@ -0,0 +1,37 @@
+function sumPrimes(num) {
+
+  получим массив простых чисел от 1 до корня из n
+  var primes_arr = [];
+
+  for (var i = 1; i < Math.sqrt(n); i++){
+    
+  }
+    
+
+  return num;
+  }
+  
+sumPrimes(10);
+
+
+
+
+
+
+
+console.log(sumPrimes(10)); // should return a number.
+console.log(sumPrimes(10)); // should return 17.
+console.log(sumPrimes(977)); // should return 73156.
+
+
+
+
+/*
+
+Sum all the prime numbers up to and including the provided number.
+
+A prime number is defined as a number greater than one and having only two divisors, one and itself. For example, 2 is a prime number because it's only divisible by one and two.
+
+The provided number may not be a prime.
+
+*/
