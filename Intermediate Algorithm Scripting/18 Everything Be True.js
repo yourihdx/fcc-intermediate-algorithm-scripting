f36036004bea3cd070cfb494bc094eac62aad7df
yourihdx <yourihdx@gmail.com>
2020-11-08 11:14:37 +0800

    evaluating every element of a collection

---
+++ Intermediate Algorithm Scripting/18 Everything Be True.js
@@ -0,0 +1,26 @@
+function truthCheck(collection, pre) {
+
+    const result = collection.filter((element)=>(element[pre])?element:null);
+
+    return collection.length === result.length;
+  }
+  
+console.log(truthCheck([{"user": "Tinky-Winky", "sex": "male"}, {"user": "Dipsy", "sex": "male"}, {"user": "Laa-Laa", "sex": "female"}, {"user": "Po", "sex": "female"}], "sex"));
+
+console.log(truthCheck([{"user": "Tinky-Winky", "sex": "male"}, {"user": "Dipsy", "sex": "male"}, {"user": "Laa-Laa", "sex": "female"}, {"user": "Po", "sex": "female"}], "sex")); // should return true.
+
+console.log(truthCheck([{"user": "Tinky-Winky", "sex": "male"}, {"user": "Dipsy"}, {"user": "Laa-Laa", "sex": "female"}, {"user": "Po", "sex": "female"}], "sex")); // should return false.
+
+truthCheck([{"user": "Tinky-Winky", "sex": "male", "age": 0}, {"user": "Dipsy", "sex": "male", "age": 3}, {"user": "Laa-Laa", "sex": "female", "age": 5}, {"user": "Po", "sex": "female", "age": 4}], "age") // should return false.
+
+truthCheck([{"name": "Pete", "onBoat": true}, {"name": "Repeat", "onBoat": true}, {"name": "FastForward", "onBoat": null}], "onBoat") // should return false
+
+truthCheck([{"name": "Pete", "onBoat": true}, {"name": "Repeat", "onBoat": true, "alias": "Repete"}, {"name": "FastForward", "onBoat": true}], "onBoat") // should return true
+
+truthCheck([{"single": "yes"}], "single") // should return true
+
+truthCheck([{"single": ""}, {"single": "double"}], "single") // should return false
+
+truthCheck([{"single": "double"}, {"single": undefined}], "single") // should return false
+
+truthCheck([{"single": "double"}, {"single": NaN}], "single") // should return false
